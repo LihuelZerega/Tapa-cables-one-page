bb90c40a9a67f7f93ee0009d37bcf36aae85c339
LihuelZerega <zeregalihuel@gmail.com>
2024-09-13 13:44:01 -0300

    Nodemailer fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Tapa Cables | Alegre su mirada sin cables por su fachada",
+  title: "Tapacables | Alegre su mirada sin cables por su fachada",
   description: "Alegre su mirada sin cables por su fachada",
 };
 
