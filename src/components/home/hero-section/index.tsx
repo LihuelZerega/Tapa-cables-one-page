e7d07885796f8e5e80185dffccb5d16608b84b2f
LihuelZerega <zeregalihuel@gmail.com>
2024-09-18 11:24:42 -0300

    Steps fix

--- src/components/home/hero-section/index.tsx
+++ src/components/home/hero-section/index.tsx
@@ -128,8 +128,14 @@ function Index() {
             </p>
             <div className="mt-10 flex items-center lg:items-start justify-center lg:justify-start gap-x-6">
               <a
+                href="#Producto"
+                className="block lg:hidden rounded-sm bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 transition"
+              >
+                ¿Te ayudamos?
+              </a>
+              <a
                 href="#producto"
-                className="rounded-sm bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 transition"
+                className="hidden lg:blockrounded-sm bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 transition"
               >
                 ¿Te ayudamos?
               </a>
