ebcd3d6fdbc087d6bdb85e53163a37798edd1a41
LihuelZerega <zeregalihuel@gmail.com>
2025-04-23 14:08:11 -0300

    fixes

--- src/components/home/hero-section/index.tsx
+++ src/components/home/hero-section/index.tsx
@@ -180,7 +180,7 @@ function Index() {
             </h1>
             <p className="mx-auto mt-8 max-w-2xl text-xl text-gray-600">
               Empresa especializada en la ocultación de cableado en fachadas. <br />
-              <p className="text-gray-800 italic text-base mt-4">Descubre, gracias a “El Secreto”, cómo lo hacemos.</p>
+              <p className="text-gray-800 italic text-base mt-6">Descubre, gracias a “El Secreto”, cómo lo hacemos.</p>
             </p>
             <div className="mt-6 flex items-center lg:items-center justify-center lg:justify-center gap-x-6">
               <a
